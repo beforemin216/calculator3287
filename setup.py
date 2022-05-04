e76c4618ebbf79088f4736e55f48f058fd808c10
beforemin216 <100770056+beforemin216@users.noreply.github.com>
2022-05-04 09:23:13 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  nmae='yourcalculator3287',
+  version='1.0.0',
+  description='oss your calculator',
+  author='JMJ',
+  author_email='beforemin386@gmail.com',
+  url='',
+  install_requires=['pytest'],
+  long_description='oss your calculator',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
